b35c4f4d9b2a30e3d996ce80c428b09003b551b4
Faguim02 <fagnermuniz12@gmail.com>
2024-10-08 16:40:18 -0300

    feat: create service from signIn, create jwt and quary database

--- backend/src/service/UserService.ts
+++ backend/src/service/UserService.ts
@@ -1,6 +1,7 @@
-import { hash } from "bcrypt";
+import { compare, hash } from "bcrypt";
 import { UserDto } from "../data/Dtos";
 import { prisma } from "../data/prismaClient";
+import { sign } from 'jsonwebtoken';
 
 export class UserService {
 
@@ -30,6 +31,51 @@ export class UserService {
             }});
 
             return "account created";
+
+        } catch (error) {
+            return error+""
+        }
+    }
+
+    async signIn(data: UserDto) {
+        try {
+            
+            if(!data.email || !data.password) {
+                throw new Error('email or password invalide');
+            }
+
+            const isEmailExists = await prisma.user.findFirst({where: {email: data.email}});
+
+            if(!isEmailExists) {
+                throw new Error('user not found');
+            }
+
+            const comparePassord = await compare(data.password, isEmailExists.password);
+
+            if(!comparePassord) {
+                throw new Error('user not found');
+            }
+
+            const jwtSecret = process.env.SECRET_JWT as string;
+
+            const token = await sign(
+                {
+                    name: isEmailExists.name,
+                    email: isEmailExists.email
+                },
+                jwtSecret,
+                {
+                    subject: isEmailExists.id,
+                    expiresIn: '30d'
+                }
+            );
+
+            return {
+                jwt: token,
+                id: isEmailExists.id,
+                name: isEmailExists.name,
+                email: isEmailExists.email
+            }
 
         } catch (error) {
             return error+""
