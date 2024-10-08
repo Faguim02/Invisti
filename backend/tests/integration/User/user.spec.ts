d494e5384a7f0acd3a169baaacf729b27e3570f9
Faguim02 <fagnermuniz12@gmail.com>
2024-10-08 15:31:55 -0300

    test <User/user.spec.ts>: create test for router '/signIn'

--- backend/tests/integration/User/user.spec.ts
+++ backend/tests/integration/User/user.spec.ts
@@ -2,8 +2,8 @@ import request from 'supertest'
 import { server } from '../../../src'
 
 describe("User test integration", () => {
-    it("Should test function 'signUp' and return 'account created'", async()=> {
-        const app = await server
+    const app = server
+    it("Should test router '/signUp' and return 'account created'", async()=> {
 
         const data = {
             name: "jonh",
@@ -16,5 +16,19 @@ describe("User test integration", () => {
         .send(data)
 
         expect(res.body).toEqual("account created")
+    })
+
+    it("Should test router '/signIn' and return the JWT", async() => {
+        
+        const data = {
+            email: "jonh@test",
+            password: "12345678"
+        }
+
+        const res = await request(app)
+        .post('/signIn')
+        .send(data)
+
+        expect(res.body).toHaveProperty("jwt")
     })
 })
