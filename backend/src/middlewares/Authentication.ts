21de1a041c9d99a073ef2e76a292a887fa993f26
Faguim02 <fagnermuniz12@gmail.com>
2024-10-08 16:40:37 -0300

    bin

---
+++ backend/src/middlewares/Authentication.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+async function Authentication(req: Request, res: Response, next: NextFunction) {
+    try {
+
+        
+        
+    } catch (error) {
+        
+    }
+}
