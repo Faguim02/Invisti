14ada4285113cfc89fa3122c256f320d4db16261
Faguim02 <fagnermuniz12@gmail.com>
2024-10-08 13:37:33 -0300

    test: create first test

--- backend/tests/integration/User/user.spec.ts
+++ backend/tests/integration/User/user.spec.ts
@@ -5,9 +5,15 @@ describe("User test integration", () => {
     it("Should test function 'signUp' and return 'account created'", async()=> {
         const app = await server
 
+        const data = {
+            name: "jonh",
+            email: "jonh@test",
+            password: "12345678"
+        }
+
         const res = await request(app)
         .post('signUp')
-        .send({})
+        .send(data)
 
         expect(res.body).toEqual("account created")
     })
