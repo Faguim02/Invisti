f363fb81d16e2569e7aa5b6f8dbb77203e7090c6
Faguim02 <fagnermuniz12@gmail.com>
2024-10-16 12:28:28 -0300

    feat: create a link components

---
+++ frontend/src/components/link/Link.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import style from './style.module.css'
+
+interface LinkProps extends React.HTMLAttributes<HTMLLinkElement> {
+    customProps?: string,
+    href: string
+}
+
+export default function LinkComponent({href, children, ...rest}: LinkProps) {
+    return <Link to={href} className={style.link}>{children}</Link>
+}
