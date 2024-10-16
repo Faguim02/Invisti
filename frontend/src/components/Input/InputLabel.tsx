782e1e3806f2fbf1157716a36373bf3c82859545
Faguim02 <fagnermuniz12@gmail.com>
2024-10-16 12:27:51 -0300

    feat: create input and inputgroup components

--- frontend/src/components/Input/InputLabel.tsx
+++ frontend/src/components/Input/InputLabel.tsx
@@ -3,14 +3,18 @@ import style from './style.module.css'
 
 interface InputLabelProps {
     labeText: string,
-    pleaceholder: string
+    pleaceholder: string,
+    value: string,
+    type?: string,
+    required?: boolean,
+    onChange: (e: any) => any
 }
 
-export default function InputLabel({labeText, pleaceholder}: InputLabelProps) {
+export default function InputLabel({labeText, pleaceholder, value, type, required, onChange}: InputLabelProps) {
     return (
         <div className={style.inputLabel}>
             <label>{labeText}</label>
-            <Input placeholder={pleaceholder}/>
+            <Input placeholder={pleaceholder} onChange={onChange} value={value} type={type} required={required}/>
         </div>
     )
 }
