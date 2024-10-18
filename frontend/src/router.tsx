fbee48c0981f83ac3eb8830003dd817bfbdd7662
Faguim02 <fagnermuniz12@gmail.com>
2024-10-17 21:56:34 -0300

    feat: create router / home

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInPage from './pages/signInPage/SignInPage'
 import SignUpPage from "./pages/signUpPage/SignUpPage";
+import Home from "./pages/home/Home";
 
 export function Router() {
     return (
@@ -10,6 +11,8 @@ export function Router() {
             <Routes>
                 <Route path="/signIn" element={<SignInPage/>}/>
                 <Route path="/signUp" element={<SignUpPage/>}/>
+
+                <Route path="/" element={<Home/>}/>
             </Routes>
         
         </BrowserRouter>
