943eb5910aaddd47a8e7b0a6a73ecf19a1b98713
Faguim02 <fagnermuniz12@gmail.com>
2024-10-20 19:57:02 -0300

    feat: add provider from react-query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from './service/react-query/QueryClient.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>        
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </StrictMode>,
 )
