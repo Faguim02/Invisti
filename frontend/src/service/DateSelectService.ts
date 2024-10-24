cbf3a4767d97ff1f5ee4525afd35083864afa8f0
Faguim02 <fagnermuniz12@gmail.com>
2024-10-23 21:16:33 -0300

    refactor: new regre negoce from converterMOnth and create isDateSelectedExists function

--- frontend/src/service/DateSelectService.ts
+++ frontend/src/service/DateSelectService.ts
@@ -5,92 +5,30 @@ type DateSelectType = {
 
 export class DateSelectService {
 
-    private covertMonthTextInNumber(month: string): number {
-        switch (month) {
-            case "jan":
-                return 1
-                break;
-            case "fev":
-                return 2
-                break;
-            case "mar":
-                return 3
-                break;
-            case "abr":
-                return 4
-                break;
-            case "mai":
-                return 5
-                break;
-            case "jun":
-                return 6
-                break;
-            case "jul":
-                return 7
-                break;
-            case "ago":
-                return 8
-                break;
-            case "set":
-                return 9
-                break;
-            case "out":
-                return 10;
-                break;
-            case "nov":
-                return 11
-                break;
-            case "dez":
-                return 12
-                break;        
-            default:
-                return 0
-                break;
+    isDateSelectedExist(month: number, year: number) {
+        const yearActual = new Date().getFullYear()
+        const monthActual = new Date().getMonth() + 1
+
+        if(year == yearActual) {
+            if(month > monthActual) {
+                return false
+            }
         }
+
+        return true
+    }
+
+    covertMonthTextInNumber(month: string): number {
+
+        const monthList = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+
+        return monthList.indexOf(month) + 1;
     }
 
     private covertMonthNumberInText(month: number): string {
-        switch (month) {
-            case 1:
-                return "jan"
-                break;
-            case 2:
-                return "fev"
-                break;
-            case 3:
-                return "mar"
-                break;
-            case 4:
-                return "abr"
-                break;
-            case 5:
-                return "mai"
-                break;
-            case 6:
-                return "jun"
-                break;
-            case 7:
-                return "jul"
-                break;
-            case 8:
-                return "ago"
-                break;
-            case 9:
-                return "set"
-                break;
-            case 10:
-                return "out";
-                break;
-            case 11:
-                return "nov"
-                break;
-            case 12:
-                return "dez"
-                break;        
-            default:
-                return ""
-                break;
-        }
+        const monthList = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+
+        return monthList[month - 1]
     }
 
     nextMonth(month: string, year: number): DateSelectType {
