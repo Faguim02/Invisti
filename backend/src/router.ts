1db1c29c8164d6a7509c68692b089b19870a09a6
Faguim02 <fagnermuniz12@gmail.com>
2025-01-30 14:49:14 -0300

    ✨ feat: criando a documentação das rotas de authenticação

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -7,7 +7,95 @@ import { ExpenseController } from "./controller/ExpenseController";
 
 export const router = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Autenticação
+ *   description: Rotas relacionadas à autenticação do usuário.
+ */
+
+/**
+ * @swagger
+ * /signUp:
+ *   post:
+ *     summary: Cria um novo usuário.
+ *     description: Cria um novo usuário com os dados fornecidos.
+ *     requestBody:
+ *      required: true
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          name: 
+ *           type: string
+ *           example: John Doe
+ *           description: Nome do usuário.
+ *          email:
+ *           type: string
+ *           example: jonh@email.com
+ *           description: Email do usuário.
+ *          password:
+ *           type: string
+ *           example: 12345678
+ *           description: Senha do usuário.
+ *     responses:
+ *       200:
+ *         description: Usuário criado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: account created.
+ */
 router.post('/signUp', new UserController().signUp);
+
+/**
+ * @swagger
+ * /signIn:
+ *   post:
+ *     summary: Autentica um usuário.
+ *     description: Autentica um usuário com os dados fornecidos.
+ *     requestBody:
+ *      required: true
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          email:
+ *           type: string
+ *           example: john@gmail.com
+ *           description: Email do usuário.
+ *          password:
+ *           type: string
+ *           example: 12345678
+ *           description: Senha do usuário.
+ *     responses:
+ *      200:
+ *       description: Usuário autenticado com sucesso.
+ *       content:
+ *        application/json:
+ *         schema:
+ *          type: object
+ *          properties:
+ *           jwt: 
+ *            type: string
+ *            description: Token de authenticação
+ *            example: code
+ *           id: 
+ *            type: string
+ *            description: id do usuario
+ *            example: 7e9d16ec-52ee-44ee-9608-cc59a1091202
+ *           name:
+ *            type: string
+ *            description: nome do usuario
+ *            example: john
+ *           email:
+ *            type: string
+ *            description: email do usuario
+ *            example: john@gmail.com
+ */
 router.post('/signIn', new UserController().signIn);
 
 router.post('/money', isAuthentication, new ReceiveMoneyController().insertFirstMoney);
