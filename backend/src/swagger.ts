6ac9bef6fe0935c79a0089f476c4cb15d340fd7d
Faguim02 <fagnermuniz12@gmail.com>
2025-01-30 21:31:33 -0300

    ♻️ refactor: alterando tags

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -10,6 +10,24 @@ const options: swaggerJsdoc.Options = {
       version: '1.0.0',
       description: 'Api de controle de finanças',
     },
+    tags: [
+      {
+        name: 'Autenticação',
+        description: 'Rotas relacionadas à autenticação de usuários',
+      },
+      {
+        name: 'Receita',
+        description: 'Rotas relacionadas à gestão de saldos',
+      },
+      {
+        name: 'Ganhos',
+        description: 'Rotas relacionadas os ganhos do usuário',
+      },
+      {
+        name: 'Gastos',
+        description: 'Rotas relacionadas os gastos do usuário',
+      },
+    ],
     servers: [
       {
         url: 'http://localhost:3000', // URL base da sua API
@@ -25,23 +43,54 @@ const options: swaggerJsdoc.Options = {
         },
       },
       schemas: {
-        User: {
+        BalanceResponse: {
           type: 'object',
           properties: {
             id: {
-              type: 'integer',
-              example: 1,
-            },
-            name: {
               type: 'string',
-              example: 'John Doe',
+              format: 'uuid',
+              example: '8a109e53-9743-450a-9390-ff7537e34aa8',
             },
-            email: {
+            balance: {
               type: 'string',
-              example: 'john.doe@example.com',
+              example: '1000',
+            },
+            date: {
+              type: 'string',
+              format: 'date-time',
+              example: '2025-01-29T20:53:30.486Z',
+            },
+            user_id: {
+              type: 'string',
+              format: 'uuid',
+              example: '7e9d16ec-52ee-44ee-9608-cc59a1091202',
             },
           },
-        }
+        },
+        BalanceItem: {
+          type: 'object',
+          properties: {
+            id: {
+              type: 'string',
+              format: 'uuid',
+              example: 'aea22880-d832-44fa-9309-e47590080f88',
+            },
+            balance: {
+              type: 'string',
+              example: '100',
+            },
+            date: {
+              type: 'string',
+              format: 'date-time',
+              example: '2024-10-10T22:47:03.752Z',
+            },
+            user_id: {
+              type: 'string',
+              format: 'uuid',
+              example: '3057d89f-2056-4265-9fe6-10b45f0240ce',
+            },
+          },
+        },
       }
     },
     security: [
