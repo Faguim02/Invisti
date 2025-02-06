702f6533702551e3fee731f74c8810164a991a7e
Faguim02 <fagnermuniz12@gmail.com>
2025-02-05 21:47:44 -0300

    💄 style: ajuste na interface ui da calculadora

--- frontend/src/pages/calculatorPage/CalculatorPage.tsx
+++ frontend/src/pages/calculatorPage/CalculatorPage.tsx
@@ -7,6 +7,7 @@ import Button from '../../components/button/Button';
 import Range from '../../components/range/Range';
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { CalculatorService } from '../../service/CalculatorService';
+import { FormatMoneyService } from '../../service/FormatMoneyService';
 
 export default function CalculatorPage() {
     
@@ -23,6 +24,10 @@ export default function CalculatorPage() {
 
     const [finalValueMonth, setFinalValueMonth] = useState<number>(0);
 
+    const [finalMoney, setFinalMoney] = useState<Record<string, number>>({});
+
+    const formatMoney = new FormatMoneyService();
+
     async function handleSubmitFinalMoney(e: any) {
         e.preventDefault();
 
@@ -33,22 +38,24 @@ export default function CalculatorPage() {
                 time: time
             });
 
-            console.log(res);
+            setFinalMoney(res as Record<string, number>);
         } else {
             const res = await new CalculatorService().finalMoneyMonth({
-                money: Number(valueMonth + valueFull),
+                money: Number(Number(valueMonth) + Number(valueFull)),
                 interestRate: tax,
                 time: time
             });
 
-            console.log(res);
+            setFinalMoney(res as Record<string, number>);
         }
+        setPage(4);
 
     }
 
     return (
         <>
             <HeaderComponent/>
+            <p>{formatMoney.formatMoney(123456.1)}</p>
             <div className={style.container}>
 
                 {page === 0 && (
@@ -109,6 +116,50 @@ export default function CalculatorPage() {
                             <InputLabel labeText="Taxa juros" pleaceholder="12%" value="" type="number" required onChange={() => {}}/>
                             <Button style={{width: '100%'}}>Simular</Button>
                         </form>
+                    </>
+                )}
+
+                {page === 4 && (
+                    <>
+                        <h2>Investimento</h2>
+                        <article className={style.resultInvistment}>
+                            <section>
+                                <p>Valor investido:</p>
+                                <span>R$ {Number(finalValue).toFixed(2)}</span>
+                            </section>
+                            <section>
+                                <p>Valor investido mensalmente:</p>
+                                <span>R$ {Number(finalMonth).toFixed(2)}</span>
+                            </section>
+                            <section>
+                                <p>Tempo investido:</p>
+                                <span>{finalTime} meses</span>
+                            </section>
+                            <section>
+                                <p>Taxa de juros:</p>
+                                <span>{tax} %</span>
+                            </section>
+                        </article>
+                        <h2>Resultado</h2>
+                        <article className={style.resultInvistment}>
+                            <section>
+                                <p>Valor investido</p>
+                                <span>R$ {Number(finalMoney?.invistedFull).toFixed(2)}</span>
+                            </section>
+                            <section>
+                                <p>Valor bruto</p>
+                                <span>R$ {Number(finalMoney?.valueBrute).toFixed(2)}</span>
+                            </section>
+                            <section>
+                                <p>Valor real</p>
+                                <span>R$ {Number(finalMoney?.finalMoney).toFixed(2)}</span>
+                            </section>
+                            <section>
+                                <p>Lucro</p>
+                                <span>R$ {Number(finalMoney?.profit).toFixed(2)}</span>
+                            </section>
+                        </article>
+                    
                     </>
                 )}
                 
