842d3c240f11d1b80896801c9b58052f9102e742
Faguim02 <fagnermuniz12@gmail.com>
2025-02-09 15:06:45 -0300

    ✨ feat: adicionando propriedade para indentificar se o saldo é positivo ou negativo

--- frontend/src/pages/home/Card.tsx
+++ frontend/src/pages/home/Card.tsx
@@ -1,21 +1,26 @@
-import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 import style from './style.module.css'
+import { FormatMoneyService } from "../../service/FormatMoneyService";
 
 interface CardProps {
     type: string,
     description: string,
     amount: number,
-    date: string
+    date: string,
+    isNegative?: boolean
 }
 
-export default function Card({description, amount, date}:CardProps) {
+export default function Card({description, amount, date, isNegative}:CardProps) {
+
+    const formatMoney = new FormatMoneyService();
+
     return (
         <li>
             <div className={style.icon}>
-                <FaArrowUp color="#29BF12" size={20}/>
+                {isNegative ? <FaArrowDown color="#FF0000" size={20}/> : <FaArrowUp color="#29BF12" size={20}/>}
                 <div>
                     <span className={style.description}>{description}</span>
-                    <span className={style.value}>R$ {amount}</span>
+                    <span className={style.value}>R$ {formatMoney.formatMoney(Number(amount))}</span>
                 </div>
             </div>
             <data className={style.date}>
