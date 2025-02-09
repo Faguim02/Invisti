a6742c50eab0b16ea0da661d83ba91f875c7207e
Faguim02 <fagnermuniz12@gmail.com>
2025-02-09 15:04:39 -0300

    ✨ feat: adicionando propriedade para alterar butão de sair e entrar

--- frontend/src/components/header/HeaderComponent.tsx
+++ frontend/src/components/header/HeaderComponent.tsx
@@ -2,14 +2,19 @@ import { useNavigate } from 'react-router-dom'
 import logo from '../../assets/LOGO.svg'
 import style from './style.module.css'
 
-export default function HeaderComponent() {
+interface Props {
+    isAuthenticated?: boolean
+}
+
+export default function HeaderComponent({isAuthenticated}:Props) {
 
     const navigate = useNavigate()
 
     return (
         <header className={style.HeaderComponent}>
             <img src={logo} alt="logo" />
-            <button onClick={() => navigate('/signUp')}>Criar conta</button>
+
+            {isAuthenticated ? <button onClick={() => navigate('/home')}>Sair</button> : <button onClick={() => navigate('/signUp')}>Criar conta</button>}
         </header>
     )
 }
