9c97de35cb8e5b7e77f2545569f5a7f92ad407e6
Faguim02 <fagnermuniz12@gmail.com>
2025-02-09 19:14:06 -0300

    ✨ feat: criando formatador de datas

--- frontend/src/service/FormatMoneyService.ts
+++ frontend/src/service/FormatMoneyService.ts
@@ -2,4 +2,15 @@ export class FormatMoneyService {
     public formatMoney(number: number): string {
         return number.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
     }
+
+    
+    public formatDate(dateString: string): string {
+        const date = new Date(dateString)
+        const monthList = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+        const day = `0${date.getDate()}`.slice(-2);
+        const month = monthList[date.getMonth()];
+        const year = date.getFullYear();
+
+        return `${day} ${month} ${year}`
+    }
 }
